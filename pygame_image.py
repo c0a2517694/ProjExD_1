6739ad519bcd328362f8db3288988dd41edb454f
c0a25176 <c0a2517694@edu.teu.ac.jp>
2026-06-09 16:36:37 +0900

    かだい1:こうかとん画像のうごき

--- pygame_image.py
+++ pygame_image.py
@@ -20,17 +20,18 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
         key_list = pg.key.get_pressed()
         if key_list[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_list[pg.K_DOWN]:
             kk_rct.move_ip(0,1)
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
-        
+        else:
+            kk_rct.move_ip(-1,0)
+
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
